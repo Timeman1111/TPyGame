a5770ce79d1c1c344fa66f6c62c5ab4528d97477
Timeman1111 <eqs5831@psu.edu>
2026-05-03 02:10:35 -0400

    Refactor `random_video_player.py` to optimize screen refresh and clean unused variables; update `.idea/workspace.xml` to refine metadata

--- scripts/random_video_player.py
+++ scripts/random_video_player.py
@@ -28,7 +28,6 @@ def test_video():
 
 def main():
     ts = tpy.render.Screen()
-    size = 240
 
     out_size = 0.25
     video_position = (0, 0)
@@ -39,15 +38,14 @@ def main():
     fps = vid_cap.get(cv2.CAP_PROP_FPS)
 
 
-    fps_wait_time = 1 / fps
 
     width = vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
     height = vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
 
     vid = tpy.video.Video(x=video_position[0], y=video_position[1], width=int(width), height=int(height))
+
+
     count = 0
-
-
     while vid_cap.isOpened():
         count += 1
         ret, frame = vid_cap.read()
@@ -63,7 +61,7 @@ def main():
 
 
         vid.draw(ts)
-        ts.refresh(force_full=True)
+        ts.refresh(force_full=False)
 
 
 
