4fe2879c32e94d2a4a98407c93c3b8bd702dc506
Ethan Small <timeman2222@gmail.com>
2026-05-04 01:51:21 -0400

    Update module-level docstring in `file` package to include `WhiteList` and improved description of functionality

--- src/tpygame/file/__init__.py
+++ src/tpygame/file/__init__.py
@@ -1,5 +1,10 @@
 """
-This module provides file management utilities.
+This module provides functionality related to file management and whitelisting.
+
+The module includes utilities for managing files through the `FileManager` and
+handling whitelisted entities using the `WhiteList`. These components facilitate
+performing file-related operations and validation processes based on a predefined
+whitelist logic.
 """
 from .fm import FileManager
 from .whitelist import WhiteList
